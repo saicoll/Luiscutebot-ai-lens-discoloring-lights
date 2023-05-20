a463b3f396e6c2809b4cc6f0cc2bd9c2d3d7eac0
saicoll <59884983+saicoll@users.noreply.github.com>
2023-05-20 11:23:06 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -22,11 +22,13 @@ PlanetX_AILens.switchfunc(PlanetX_AILens.FuncList.Color)
 let strip = neopixel.create(DigitalPin.P15, 2, NeoPixelMode.RGB)
 basic.forever(function () {
     PlanetX_AILens.cameraImage()
-    if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.blue)) {
-        radio.sendNumber(200)
-        basic.pause(6000)
-        cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
-        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    if (AUXILIAR == 11) {
+        if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.blue)) {
+            radio.sendNumber(200)
+            basic.pause(6000)
+            cuteBot.colorLight(cuteBot.RGBLights.ALL, 0x0000ff)
+            strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+        }
     }
     if (AUXILIAR == 12) {
         if (PlanetX_AILens.colorCheck(PlanetX_AILens.ColorLs.red)) {
